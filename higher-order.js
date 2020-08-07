bd6f4da767cd3b22c7997087cbc6f401694e9c49
Jake Butters <jacobbutters@gmail.com>
2020-08-07 16:12:07 -0600

    completed higher-order

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,11 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(function(element, index, array){
+  if(element % 2 === 0){
+    return true
+  }
+})
 
 
 
@@ -44,7 +48,11 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map((element, index, array) => {
+  // let newArray = element * 1.07
+  // return newArray
+  return element * 1.07
+});
 
 
 
@@ -63,22 +71,41 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce((acc, el, index, array)=>{
+  return (acc += el)
+}, 0)
 
 
 
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
-const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulbabunny","CP":135},
-{"monster":"Bulbabunny","CP":250},{"monster":"Ponylopse","CP":277},{"monster":"Ponylopse","CP":184},
-{"monster":"Pikadoughnet","CP":207},{"monster":"Bulbabunny","CP":139},{"monster":"Pikadoughnet","CP":47},
-{"monster":"Pikadoughnet","CP":175},{"monster":"WaterHorsia","CP":26},{"monster":"Ponylopse","CP":19},
-{"monster":"Pikadoughnet","CP":218},{"monster":"Charaflier","CP":101},{"monster":"WaterHorsia","CP":96},
-{"monster":"Pikadoughnet","CP":253},{"monster":"Sandmush","CP":146},{"monster":"Bulbabunny","CP":247},
-{"monster":"Charaflier","CP":55},{"monster":"Bulbabunny","CP":72},{"monster":"Pikadoughnet","CP":300},
-{"monster":"Sandmush","CP":262},{"monster":"Sandmush","CP":25},{"monster":"Charaflier","CP":215},
-{"monster":"Ponylopse","CP":125},{"monster":"Bulbabunny","CP":178}];
+const monstersInYourPocket = [
+{"monster":"Bulbabunny","CP":156},
+{"monster":"Bulbabunny","CP":135},
+{"monster":"Bulbabunny","CP":250},
+{"monster":"Ponylopse","CP":277},
+{"monster":"Ponylopse","CP":184},
+{"monster":"Pikadoughnet","CP":207},
+{"monster":"Bulbabunny","CP":139},
+{"monster":"Pikadoughnet","CP":47},
+{"monster":"Pikadoughnet","CP":175},
+{"monster":"WaterHorsia","CP":26},
+{"monster":"Ponylopse","CP":19},
+{"monster":"Pikadoughnet","CP":218},
+{"monster":"Charaflier","CP":101},
+{"monster":"WaterHorsia","CP":96},
+{"monster":"Pikadoughnet","CP":253},
+{"monster":"Sandmush","CP":146},
+{"monster":"Bulbabunny","CP":247},
+{"monster":"Charaflier","CP":55},
+{"monster":"Bulbabunny","CP":72},
+{"monster":"Pikadoughnet","CP":300},
+{"monster":"Sandmush","CP":262},
+{"monster":"Sandmush","CP":25},
+{"monster":"Charaflier","CP":215},
+{"monster":"Ponylopse","CP":125},
+{"monster":"Bulbabunny","CP":178}];
 // Do not edit the code above.
 
 /*
@@ -89,7 +116,11 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter((element, index, array)=>{
+  if(element.CP > 200){
+    return true
+  }
+})
 
 
 
@@ -106,7 +137,10 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map((element, index, array)=>{
+  return (element.price * element.tax) + element.price
+})
+
 
 
 
@@ -126,6 +160,21 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+// let bobsTotal 
+
+
+let getBob = purchases.filter((element, index, array)=>{
+  if(element.owner === 'Bob'){
+    return true
+  }
+})
+
+// console.log(getBob)
+
+let bobsTotal = getBob.reduce((acc, element, index, array)=>{
+  return (acc += element.price)
+}, 0)
+
+
 
 
